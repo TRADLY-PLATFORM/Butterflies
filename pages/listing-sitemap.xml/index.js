7f30b27324fabeb037d442e3c1522e840e534ca4
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-11-22 23:12:52 +0600

    sitemap updated

---
+++ pages/listing-sitemap.xml/index.js
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { GetServerSideProps } from 'next';
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
+import tradly from 'tradly';
+
+export const getServerSideProps = async (ctx) => {
+  var all_listings = [];
+
+  const response = await tradly.app.getListings({
+    bodyParam: { page: 1 },
+  });
+  const total_records = await response.data.total_records;
+
+  const totalPages = Math.ceil(total_records / 10);
+
+  let fields ;
+  // listings.map((listing) => ({
+  //   loc: `${process.env.SITE_URL}/listing/${listing.id}`,
+  //   lastmod: new Date().toISOString(),
+  //   changefreq: 'daily',
+  //   priority: 0.7,
+  // }));
+  for (let index = 0; index < totalPages; index++) {
+
+    const response = await tradly.app.getListings({
+      bodyParam: { page: index + 1, per_page: 10 },
+    });
+
+    const listings = await response.data.listings;
+    all_listings.push(...listings);
+
+   
+  }
+ 
+
+  fields = all_listings.map((listing) => ({
+    loc: `${process.env.SITE_URL}/listing/${listing.id}`,
+    lastmod: new Date().toISOString(),
+    changefreq: 'daily',
+    priority: 0.7,
+  }));
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+export default function Site() {}
