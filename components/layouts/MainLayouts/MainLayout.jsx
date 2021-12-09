165a3249f9d7ca2106688b7bd335bcabb889dad9
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-12-09 16:35:54 +0600

    fix/client_issues

--- components/layouts/MainLayouts/MainLayout.jsx
+++ components/layouts/MainLayouts/MainLayout.jsx
@@ -20,9 +20,9 @@ const MainLayout = ({ children, pageTitle, pageDescription }) => {
         <div className="bg-[#f6f9ff] min-h-screen  overflow-x-hidden px-[50px]  2xl:px-[12%] pt-5 pb-5">
           <div>{children}</div>
         </div>
-        <div className="bg-wite min-h-[200px]   left-0 bottom-0 px-[50px]  2xl:px-[12%]">
+        {/* <div className="bg-wite min-h-[200px]   left-0 bottom-0 px-[50px]  2xl:px-[12%]">
           <Footer />
-        </div>
+        </div> */}
       </div>
 
       <div className=" md:hidden">
@@ -32,9 +32,9 @@ const MainLayout = ({ children, pageTitle, pageDescription }) => {
         <div className="w-screen min-h-screen bg-[#f6f9ff] pt-[24px] px-[16px] pb-10">
           {children}
         </div>
-        <div className="bg-wite min-h-[200px]  left-0 bottom-0 px-[16px] xs:px-[35px] ">
+        {/* <div className="bg-wite min-h-[200px]  left-0 bottom-0 px-[16px] xs:px-[35px] ">
           <Footer />
-        </div>
+        </div> */}
       </div>
     </>
   );
