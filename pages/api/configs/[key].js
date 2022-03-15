b97da51378a83b0183bd501f913762ad72e28e6f
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2022-03-15 20:12:37 +0600

    sdk init.. updated

--- pages/api/configs/[key].js
+++ pages/api/configs/[key].js
@@ -3,19 +3,19 @@ import tradly from 'tradly';
 
 export default async function handler(req, res) {
   const { key } = req.query;
-   tradly.init
-     .config({
-       token: process.env.API_KEY,
-       environment: process.env.ENVIRONMENT,
-     })
-     .then(async (response) => {
-       const response = await tradly.app.getConfigList({
-         paramBody: key,
-       });
-       if (!response.error) {
-         res.status(200).send(response.data);
-       } else {
-         res.status(500).send(response.error);
-       }
-     });
+  tradly.init
+    .config({
+      token: process.env.API_KEY,
+      environment: process.env.ENVIRONMENT,
+    })
+    .then(async () => {
+      const response = await tradly.app.getConfigList({
+        paramBody: key,
+      });
+      if (!response.error) {
+        res.status(200).send(response.data);
+      } else {
+        res.status(500).send(response.error);
+      }
+    });
 }
