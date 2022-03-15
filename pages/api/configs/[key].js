b26a6bd019f91ce1854e1c5531003feb92ad9f48
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2022-03-15 20:02:42 +0600

    sdk init.. updated

--- pages/api/configs/[key].js
+++ pages/api/configs/[key].js
@@ -3,7 +3,17 @@ import tradly from 'tradly';
 
 export default async function handler(req, res) {
   const { key } = req.query;
-
+  tradly.init
+    .config({
+      token: process.env.API_KEY,
+      environment: process.env.ENVIRONMENT,
+    })
+    .then((response) => {
+      res
+        .status(200)
+        .send({ status: 'connected', host_name: req.headers.host });
+    });
+  
   const response = await tradly.app.getConfigList({
     paramBody: key,
   });
